cae9d6b8fe3f01b9b51796e60e87500c31eccb3e
bchae <12506628+bonchae@users.noreply.github.com>
2023-11-27 12:18:25 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 # create new conda env
-conda create -n SAA python=3.9
-source activate SAA
+#conda create -n SAA python=3.9
+#source activate SAA
 
 # PyTorch
 pip3 install torch==1.10.0+cu113 torchvision==0.11.1+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
 
 # $ProjectRoot: the root you save our project, e.g., /home/anyad/VAND-solution
-ProjectRoot=/home/anyad/VAND-solution
+ProjectRoot=/home/ec2-user/Segment-Any-Anomaly
 cd $ProjectRoot
 
 # SAM and DINO
